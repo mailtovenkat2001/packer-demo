4bb4331c04f0093390e19cce2307656f6e981363
mailtovenkat2001 <37097353+mailtovenkat2001@users.noreply.github.com>
2020-05-19 00:16:40 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import boto3
+iam = boto3.resource('iam',aws_access_key_id='AKIAVOJJABES6ZKLQAXN',aws_secret_access_key='Ey49Dpcn5
+Cs9B4kTa8iH+uIf8CIdw9g9kpe+XQgG')
+
+created_user = iam.create_user(
+            UserName='jenkinstest432'
+            )
+print(created_user)
