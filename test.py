be0ff0220e8e8ea88fb0d657506e08c19d0ebaec
mailtovenkat2001 <37097353+mailtovenkat2001@users.noreply.github.com>
2020-05-19 00:25:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import boto3
-iam = boto3.resource('iam',aws_access_key_id='AKIAVOJJABES6ZKLQAXN',aws_secret_access_key='Ey49Dpcn5
-Cs9B4kTa8iH+uIf8CIdw9g9kpe+XQgG')
+iam = boto3.resource('iam',aws_access_key_id='AKIAVOJJABES6ZKLQAXN',aws_secret_access_key='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')
 
 created_user = iam.create_user(
             UserName='jenkinstest432'
