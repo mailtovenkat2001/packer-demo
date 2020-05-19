338d76ef07a1e6a728937ada281e43e4288e61d7
mailtovenkat2001 <37097353+mailtovenkat2001@users.noreply.github.com>
2020-05-19 08:11:12 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-import boto3
-iam = boto3.resource('iam',aws_access_key_id='AKIAVOJJABES6ZKLQAXN',aws_secret_access_key='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')
 
-created_user = iam.create_user(
-            UserName='jenkinstest432'
-            )
-print(created_user)
+import boto3
+
+# Create IAM client
+iam =boto3.client('iam',aws_access_key_id="AKIASAXBLKACHTCP4Z4K",aws_secret_access_key="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")
+
+# Create user
+response = iam.create_user(
+    UserName='venkat-test'
+)
+
+print(response)
